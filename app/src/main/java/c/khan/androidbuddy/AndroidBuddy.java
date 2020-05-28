298c702efe2afa3f621301643ef8e5a1eb3c7e1c
Muhammad Sakib Khan <sakib2131@gmail.com>
2020-05-28 17:16:12 +0600

    Body and Leg Fragments added. App now dynamically calls the cartoon

--- app/src/main/java/c/khan/androidbuddy/AndroidBuddy.java
+++ app/src/main/java/c/khan/androidbuddy/AndroidBuddy.java
@@ -11,12 +11,37 @@ public class AndroidBuddy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_buddy);
 
+        FragmentManager fragManager = getSupportFragmentManager();
+
         BodyPartFragment headFrag = new BodyPartFragment();
 
-        FragmentManager fragManager = getSupportFragmentManager();
+        headFrag.setmImageIds(ImageAssets.getHeads());
+        headFrag.setListIndex(1);
 
         fragManager.beginTransaction()
                 .add(R.id.head_container, headFrag)
+                .commit();
+
+
+
+        BodyPartFragment bodyfrag = new BodyPartFragment();
+
+        bodyfrag.setmImageIds(ImageAssets.getBodies());
+        bodyfrag.setListIndex(1);
+
+        fragManager.beginTransaction()
+                .add(R.id.body_container, bodyfrag)
+                .commit();
+
+
+
+        BodyPartFragment legfrag = new BodyPartFragment();
+
+        legfrag.setmImageIds(ImageAssets.getLegs());
+        legfrag.setListIndex(1);
+
+        fragManager.beginTransaction()
+                .add(R.id.leg_container, legfrag)
                 .commit();
 
     }
--- app/src/main/java/c/khan/androidbuddy/BodyPartFragment.java
+++ app/src/main/java/c/khan/androidbuddy/BodyPartFragment.java
@@ -2,10 +2,13 @@ package c.khan.androidbuddy;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import java.util.List;
 
 
 /**
@@ -13,6 +16,11 @@ import android.widget.ImageView;
  */
 
 public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = "BodyPartFragment";
+
+    private List <Integer> mImageIds;
+    private int mListIndex;
 
     public BodyPartFragment() {
 
@@ -25,10 +33,28 @@ public class BodyPartFragment extends Fragment {
 
         ImageView imageView = (ImageView) rootView.findViewById(R.id.bodypart_imageview);
 
-        imageView.setImageResource(ImageAssets.getHeads().get(0));
+        //imageView.setImageResource(ImageAssets.getHeads().get(0));
+
+        if (mImageIds != null){
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        }
+
+        else {
+            Log.v(TAG, "Error: No image ID");
+        }
+
+
 
         return rootView;
 
+    }
+
+    public void setmImageIds(List<Integer> imageIds){
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex (int index){
+        mListIndex = index;
     }
 
 }
